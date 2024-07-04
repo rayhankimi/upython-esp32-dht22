56ec75fad2a417c0512eecdc27897b2cb4351caa
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-07-05 00:27:10 +0800

    Update main.py

--- espLCD/main.py
+++ espLCD/main.py
@@ -5,17 +5,17 @@ from i2c_lcd import I2cLcd
 import urequests
 import network
 
-# Konfigurasi I2C untuk LCD
-I2C_ADDR = 0x27  # Alamat I2C dari LCD Anda
-I2C_SDA_PIN = 21  # Pin SDA ESP32
-I2C_SCL_PIN = 22  # Pin SCL ESP32
+#I2C Settings
+I2C_ADDR = 0x27  
+I2C_SDA_PIN = 21 
+I2C_SCL_PIN = 22  
 
-# Inisialisasi I2C untuk LCD
+# I2C Init
 i2c = I2C(0, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)  # (i2c, address, row, column)
 
-firebase_url = 'https://upy-uas-default-rtdb.asia-southeast1.firebasedatabase.app/'
-firebase_secret = 'amDEG02GQmM7rPADR0SZd5OprvFCYsyCtObLF834'
+firebase_url = 'Your Firebase URL'
+firebase_secret = 'Your Firebase Secret'
 
 def initWifi(ssid, password) -> None:
     wlan = network.WLAN(network.STA_IF)
@@ -26,9 +26,9 @@ def initWifi(ssid, password) -> None:
         pass
 
 def setup() -> None:
-    # Inisialisasi wifi
-    ssid = "Anak kos"
-    password = "22445588"
+    # Init Wifi
+    ssid = "Your SSID"
+    password = "Your Password"
     initWifi(ssid, password)
 
 def getDataFromFirebase():
